a3a50f2b8f19e7ff69998e80ad59d01b6f8171fb
Özgür Deniz Türker <ozgurdenizturker@Ozgur-MacBook-Pro.local>
2024-08-20 11:07:25 +0200

    initilize spring boot project

---
+++ src/test/kotlin/com/odenizturker/locky/auth/AuthApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.odenizturker.locky.auth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AuthApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
